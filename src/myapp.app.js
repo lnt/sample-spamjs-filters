30594b8cb049ee0e428634c2aae61882e6ef5bb1
Lalit Tanwar <lalit.tanwar07@gmail.com>
2017-04-21 23:39:46 +0530

    self start

--- src/myapp.app.js
+++ src/myapp.app.js
@@ -1,58 +1,62 @@
 define({
     name: "myapp.app",
     extend: "spamjs.view",
-    modules: ["jqrouter"]
-}).as(function(app, jqrouter) {
+    modules: ["jqrouter", "jQuery"]
+}).as(function (app, jqrouter, jq) {
 
     return {
         router: jqrouter.map({
             "/app/boot/*": "openDevSection",
             "/app/search": "openHome"
         }),
-        _init_: function(config) {
+        _init_: function (config) {
             var self = this;
             jqrouter.start();
             _importStyle_("myapp/app/style");
             this.$$.loadTemplate(
                 this.path("myapp.app.html")
-            ).done(function() {
+            ).done(function () {
                 self.router().defaultRoute(config.options.defaultUrl);
             });
         },
-        openHome: function(e, name, query) {
+        openHome: function (e, name, query) {
             this.$$.find("#main").append('<spamjs-view module="app.search"></spamjs-view>');
         },
-        openDevSection: function() {
+        openDevSection: function () {
             var self = this;
-            module("spamjs.bootconfig", function(myModule) {
+            module("spamjs.bootconfig", function (myModule) {
                 self.add(myModule.instance({
                     id: "bootconfig",
                     routerBase: "/app/boot/"
                 }));
             });
         },
-        openModule: function(myModuleName, options) {
+        openModule: function (myModuleName, options) {
             var self = this;
-            bootloader.module404 = function(_moduleName) {
+            bootloader.module404 = function (_moduleName) {
                 if (myModuleName === _moduleName) {
                     self.module404(myModuleName);
                 }
             };
-            module([myModuleName], function(myModule) {
+            module([myModuleName], function (myModule) {
                 self.add(myModule.instance({
                     id: "main",
                     options: options
                 }));
             });
         },
-        module404: function(myModuleName) {
+        module404: function (myModuleName) {
             this.$$.html(myModuleName + " not found");
         },
-        _routerEvents_: function(e, target, data, params) {
+        _routerEvents_: function (e, target, data, params) {
             console.error("_routerEvents_", e, target, data, params);
         },
-        _ready_: function() {
-            //this.instance({options: {"module": "rudrax.home"}}).addTo(jQuery("body"));
+        _ready_: function () {
+            this.instance({
+                options: {
+                    defaultUrl: "/app/search"
+                }
+            }).addTo(jq("body"));
         }
     };
 
