5fb4923466557be583da5b205abe440b341d1a04
Lalit Tanwar <lalit.tanwar07@gmail.com>
2017-04-22 00:45:50 +0530

    build fix

--- build.sh
+++ build.sh
@@ -2,16 +2,13 @@
 # Set php version through phpenv. 5.3, 5.4, 5.5 & 5.6 available
 phpenv local 5.5
 
-nvm install 0.10
 npm update
 npm install -g grunt-cli
 bower update
 grunt build
 
-lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; mirror -R src/ /src"
 lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; mirror -R dist/ /dist"
-
 lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; put -O / index.html"
 lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; put -O / .htaccess"
-
+lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; mirror -R src/ /src"
 lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; mirror -R no/ /no"
